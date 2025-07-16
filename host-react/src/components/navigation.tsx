08e54824e76e94d7cce218b2d348c0e52351f079
Rodrigo Bondoc <rodrigo.bondoc@sourcetoad.com>
2025-07-16 13:35:12 -0700

    chore(host): nav - clean up and show page name

--- host-react/src/components/navigation.tsx
+++ host-react/src/components/navigation.tsx
@@ -1,23 +1,10 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { Link } from '@tanstack/react-router';
+import { Link, useLocation } from '@tanstack/react-router';
 import { X } from 'lucide-react';
-import { PropsWithChildren, ReactNode, useState } from 'react';
+import { PropsWithChildren, ReactNode, useMemo, useState } from 'react';
 import {Drawer} from 'vaul';
 import { cn } from '@/utilities/styles';
 import {useMediaQuery} from '@/hooks/use-media-query';
-
-function NavigationToolbar({ children }: PropsWithChildren): ReactNode {
-    return (
-        <div className="fixed bottom-0 inset-x-0 bg-pink-800 flex justify-between text-xs px-2 py-0.5">
-            <div className="italic">
-                <span>Viewing: </span>
-                <span>Proof of Concept</span>
-            </div>
-
-            {children}
-        </div>
-    );
-}
 
 type NavigationLink = {
     label: string;
@@ -39,77 +26,93 @@ const links: NavigationLink[] = [
     }
 ];
 
+function NavigationToolbar({ children }: PropsWithChildren): ReactNode {
+    const location = useLocation();
 
-export function Navigation(): ReactNode {
-    const [open, setOpen] = useState(false);
-    const isDesktop = useMediaQuery('(min-width: 768px)');
+    const name = useMemo(() => {
+        const link = links.find((link) => link.to === location.pathname);
 
-    if (isDesktop) {
-        return (
-            <Dialog.Root open={open} onOpenChange={setOpen}>
-                <div className="fixed bottom-0 inset-x-0 bg-pink-800 flex justify-between text-xs px-2 py-0.5">
-                    <div className="italic">
-                        <span>Viewing: </span>
-                        <span>Proof of Concept</span>
-                    </div>
+        if (link) {
+            return link.label;
+        }
 
-                    <Dialog.Trigger className="text-white underline underline-offset-3">Open Menu</Dialog.Trigger>
-                </div>
-                <Dialog.Portal>
-                    <Dialog.Overlay
-                        className="fixed inset-0 z-50 bg-black/80"
-                    />
-                    <Dialog.Content
-                        className={cn(
-                            'fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-50',
-                            'border shadow-lg rounded-md',
-                            'bg-slate-900',
-                            'p-6',
-                            'w-full max-w-lg sm:max-w-md',
-                            'data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95',
-                            'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95',
-                        )}
-                    >
-                        <div className="flex flex-col gap-y-1.5 px-4 py-2 text-center sm:text-left">
-                            <Dialog.Title className="text-lg font-semibold leading-none tracking-tight">Navigation Menu</Dialog.Title>
-                        </div>
-
-                        <nav className="flex flex-col gap-y-4 px-4 py-2 text-left">
-                            {links.map((link, index) => (
-                                <Link
-                                    className="bg-purple-600 rounded-me px-4 py-2 rounded-md"
-                                    key={index}
-                                    to={link.to}
-                                    viewTransition
-                                    onClick={() => setOpen(false)}
-                                >
-                                    {link.label}
-                                </Link>
-                            ))}
-                        </nav>
-
-                        <Dialog.Close
-                            className="absolute top-4 right-4 rounded-md opacity-70"
-                        >
-                            <X className="h-4 w-4" />
-                            <span className="sr-only">Close</span>
-                        </Dialog.Close>
-                    </Dialog.Content>
-                </Dialog.Portal>
-            </Dialog.Root>
-        );
-    }
+        return '[ERROR] Page name could not be found.';
+    }, [location.pathname])
 
     return (
-        <Drawer.Root shouldScaleBackground open={open} onOpenChange={setOpen}>
-            <div className="fixed bottom-0 inset-x-0 bg-pink-800 flex justify-between text-xs px-2 py-0.5">
-                <div className="italic">
-                    <span>Viewing: </span>
-                    <span>Proof of Concept</span>
-                </div>
-
-                <Drawer.Trigger className="text-white underline underline-offset-3">Open Menu</Drawer.Trigger>
+        <div className="fixed bottom-0 inset-x-0 bg-pink-800 flex justify-between text-xs px-2 py-0.5">
+            <div className="italic">
+                <span>Viewing: </span>
+                <span>{name}</span>
             </div>
+
+            {children}
+        </div>
+    );
+}
+
+type BaseNavigationProps = {
+    open: boolean;
+    onOpenChange: (value: boolean) => void;
+};
+
+export function DesktopNavigation({open, onOpenChange}: BaseNavigationProps): ReactNode {
+    return (
+        <Dialog.Root open={open} onOpenChange={onOpenChange}>
+            <NavigationToolbar>
+                <Dialog.Trigger className="text-white underline underline-offset-3">Open Menu</Dialog.Trigger>
+            </NavigationToolbar>
+            <Dialog.Portal>
+                <Dialog.Overlay
+                    className="fixed inset-0 z-50 bg-black/80"
+                />
+                <Dialog.Content
+                    className={cn(
+                        'fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-50',
+                        'border shadow-lg rounded-md',
+                        'bg-slate-900',
+                        'p-6',
+                        'w-full max-w-lg sm:max-w-md',
+                        'data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95',
+                        'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95',
+                    )}
+                >
+                    <div className="flex flex-col gap-y-1.5 px-4 py-2 text-center sm:text-left">
+                        <Dialog.Title className="text-lg font-semibold leading-none tracking-tight">Navigation Menu</Dialog.Title>
+                    </div>
+
+                    <nav className="flex flex-col gap-y-4 px-4 py-2 text-left">
+                        {links.map((link, index) => (
+                            <Link
+                                className="bg-purple-600 rounded-me px-4 py-2 rounded-md"
+                                key={index}
+                                to={link.to}
+                                viewTransition
+                                onClick={() => onOpenChange(false)}
+                            >
+                                {link.label}
+                            </Link>
+                        ))}
+                    </nav>
+
+                    <Dialog.Close
+                        className="absolute top-4 right-4 rounded-md opacity-70"
+                    >
+                        <X className="h-4 w-4" />
+                        <span className="sr-only">Close</span>
+                    </Dialog.Close>
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
+    );
+}
+
+export function MobileNavigation({open, onOpenChange}: BaseNavigationProps): ReactNode {
+    return (
+        <Drawer.Root shouldScaleBackground open={open} onOpenChange={onOpenChange}>
+            <NavigationToolbar>
+                <Drawer.Trigger className="text-white underline underline-offset-3">Open Menu</Drawer.Trigger>
+            </NavigationToolbar>
             <Drawer.Portal>
                 <Drawer.Overlay className="fixed inset-0 z-50 bg-black/80" />
                 <Drawer.Content
@@ -127,7 +130,7 @@ export function Navigation(): ReactNode {
                                 key={index}
                                 to={link.to}
                                 viewTransition
-                                onClick={() => setOpen(false)}
+                                onClick={() => onOpenChange(false)}
                             >
                                 {link.label}
                             </Link>
@@ -137,4 +140,15 @@ export function Navigation(): ReactNode {
             </Drawer.Portal>
         </Drawer.Root>
     );
+};
+
+export function Navigation(): ReactNode {
+    const [open, setOpen] = useState(false);
+    const isDesktop = useMediaQuery('(min-width: 768px)');
+
+    if (isDesktop) {
+        return <DesktopNavigation open={open} onOpenChange={setOpen} />;
+    }
+
+    return <MobileNavigation open={open} onOpenChange={setOpen} />;
 }
